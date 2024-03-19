7a984900a4c9221205271f0f25132a05abfdec65
stuck1a <stuck1a@stuck1a.de>
2024-03-19 12:42:41 +0100

    Added debug mode, added open gl 3.1 context

--- src/deadzone/Deadzone.java
+++ src/deadzone/Deadzone.java
@@ -6,6 +6,8 @@ import org.lwjgl.system.*;
 
 import java.nio.*;
 import java.util.Objects;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
 
 import static java.lang.Thread.sleep;
 import static org.lwjgl.glfw.Callbacks.*;
@@ -20,6 +22,8 @@ public class Deadzone {
   private long window;
   public GameTimer timer;
   public static Deadzone game;
+  
+  public boolean debugMode = false;
 
   
   /**
@@ -28,7 +32,19 @@ public class Deadzone {
    */
   public static void main(String[] args) throws InterruptedException {
     game = new Deadzone();
+    // Process arguments
+    for (String arg: args) {
+      String param = arg.toUpperCase();
+      if (Objects.equals( param, "D") || Objects.equals( param, "DEBUG")) {
+        game.debugMode = true;
+        continue;
+      }
+    }
     game.run();
+  }
+  
+  public boolean isDebugMode() {
+    return debugMode;
   }
   
   
@@ -43,16 +59,11 @@ public class Deadzone {
     // Initialize GLFW
     if ( !glfwInit() )
       throw new IllegalStateException("Unable to initialize GLFW");
-    // Init application window
-    glfwDefaultWindowHints();
-    glfwWindowHint(GLFW_VISIBLE, GLFW_FALSE); // hide until init is done
-    glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE); // not resizable
-    // Create application window and load launcher scene
-    window = glfwCreateWindow(800, 600, "Deadzone", NULL, NULL);
-    if ( window == NULL ) {
-      glfwTerminate();
-      throw new RuntimeException("Failed to create the GLFW window");
-    }
+    
+    // Set up render context
+    createRenderContext(800, 600, "Deadzone", NULL, NULL);
+    // Set up initial scene (launcher scene)
+    // TODO ...
     
     // Get thread stack and generate initial frame
     try ( MemoryStack stack = stackPush() ) {
@@ -77,7 +88,7 @@ public class Deadzone {
     // Display application window
     glfwShowWindow(window);
     // Initialize the game timer object
-    timer = new GameTimer(30);  // default value TODO: use value from stored user config, if any
+    timer = new GameTimer(30);  // default value  // TODO: use value from stored user config, if any
   }
   
   
@@ -117,7 +128,36 @@ public class Deadzone {
       // Keep the target frame rate
       sleep(timer.getSleepTime());
     }
-    
+  }
+  
+  
+  /**
+   * Set up GLFW to use the OpenGL 3.2 core profile for rendering
+   */
+  private void createRenderContext(int width, int height, String title, long monitor, long share) {
+    // Configure the window (launcher scene)
+    glfwDefaultWindowHints();
+    glfwWindowHint(GLFW_VISIBLE, GLFW_FALSE); // hide until init is done
+    glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE); // not resizable
+    // Configure OpenGL 3.2 core profile
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 2);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GLFW_TRUE);  // deactivate deprecated features
+    // Create application window
+    window = glfwCreateWindow(width, height, title, monitor, share);
+    // If the GPU doesn't support OpenGL 3.2 try 2.1 as fallback
+    if ( window == NULL ) {
+      final JPanel panel = new JPanel();
+      JOptionPane.showMessageDialog(
+        panel,
+        "It appears that your graphics card does not support OpenGL 3.1.\nUnfortunately, this means that 'Deadzone' will not run on your system.",
+        "Failed to create the GLFW window",
+        JOptionPane.ERROR_MESSAGE
+      );
+      glfwTerminate();
+      throw new RuntimeException("Failed to create the GLFW window");
+    }
   }
   
   /**
@@ -155,11 +195,19 @@ public class Deadzone {
    * Subroutine of the game loop which renders the updated data for the next frame
    */
   public void render() {
-    System.out.println("Frame:" + timer.getCurrentFrame());
-    System.out.println("Elapsed Time:" + timer.getCurrentTimestamp());
-    System.out.println("Elapsed Seconds:" + timer.getCurrentSecond());
-    System.out.println("Current FPS:" + timer.getFps());
-    System.out.println("----------------------------");
+    if (isDebugMode()) {
+      System.out.println("Elapsed Time:" + timer.getCurrentTimestamp());
+      System.out.println("Elapsed Seconds:" + timer.getCurrentSecond());
+      System.out.println("Current Frame:" + timer.getCurrentFrame());
+      System.out.println("Current FPS:" + timer.getFps());
+      System.out.println("----------------------------");
+    }
+
+    
+    
+    // DEBUG BLOCK START - RENDER A VERTICE BASED TRIANGLE
+    
+    // DEBUG BLOCK END
   }
   
 
