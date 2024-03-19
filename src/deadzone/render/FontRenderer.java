2dc431b16623232bdfcf7c13a8fbb23be160fdae
stuck1a <stuck1a@stuck1a.de>
2024-03-20 00:10:16 +0100

    added some basic shader

---
+++ src/deadzone/render/FontRenderer.java
@@ -0,0 +1,19 @@
+package deadzone.render;
+
+import java.awt.*;
+import java.io.File;
+import java.io.IOException;
+
+public class FontRenderer {
+  
+  public FontRenderer() throws IOException, FontFormatException {
+  
+    String fontPath = "";
+    File fontFile = new File(fontPath);
+    Font fontPlay = Font.createFont(Font.TRUETYPE_FONT, fontFile);
+    
+    
+    
+  }
+
+}
