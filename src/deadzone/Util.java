f5477cbe245e1a95c71fad8c9fa4c90208e17591
stuck1a <stuck1a@stuck1a.de>
2024-03-20 03:37:31 +0100

    added Shader and Util class, restructured shader package

---
+++ src/deadzone/Util.java
@@ -0,0 +1,15 @@
+package deadzone;
+
+/**
+ * Support class which provides some smart general utility functions
+ */
+public class Util {
+  
+  /**
+   * @return The root directory of this application
+   */
+  public static String getAbsoluteClientPath() {
+    return "";
+  }
+
+}
