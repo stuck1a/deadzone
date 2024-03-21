208ede735d62f083fc40859fa577d6d71711f179
stuck1a <stuck1a@stuck1a.de>
2024-03-21 08:53:49 +0100

    Reformatted shader class

--- src/deadzone/shader/Shader.java
+++ src/deadzone/shader/Shader.java
@@ -9,22 +9,18 @@ import java.io.InputStreamReader;
 import static org.lwjgl.opengl.GL11.GL_TRUE;
 import static org.lwjgl.opengl.GL20.*;
 
+
 /**
- * This class represents a shader.
- *
- * @author Heiko Brumme
+ * This class represents a shader
  */
 public class Shader {
-  
-  /**
-   * Stores the handle of the shader.
-   */
+  /** Stores the shader handle */
   private final int id;
   
+  
   /**
-   * Creates a shader with specified type. The type in the tutorial should be
-   * either <code>GL_VERTEX_SHADER</code> or <code>GL_FRAGMENT_SHADER</code>.
-   *
+   * Creates a new shader with specified type.
+   * Currently, only supports vertex (GL_VERTEX_SHADER) and fragment (GL_FRAGMENT_SHADER) shaders.
    * @param type Type of the shader
    */
   public Shader(int type) {
@@ -32,41 +28,7 @@ public class Shader {
   }
   
   /**
-   * Sets the source code of this shader.
-   *
-   * @param source GLSL Source Code for the shader
-   */
-  public void source(CharSequence source) {
-    glShaderSource(id, source);
-  }
-  
-  /** Compiles the shader and checks its status afterwards. */
-  public void compile() {
-    glCompileShader(id);
-    
-    checkStatus();
-  }
-  
-  /**
-   * Checks if the shader was compiled successfully.
-   */
-  private void checkStatus() {
-    int status = glGetShaderi(id, GL_COMPILE_STATUS);
-    if (status != GL_TRUE) {
-      throw new RuntimeException(glGetShaderInfoLog(id));
-    }
-  }
-  
-  /**
-   * Deletes the shader.
-   */
-  public void delete() {
-    glDeleteShader(id);
-  }
-  
-  /**
    * Getter for the shader ID.
-   *
    * @return Handle of this shader
    */
   public int getID() {
@@ -74,34 +36,50 @@ public class Shader {
   }
   
   /**
-   * Creates a shader with specified type and source and compiles it. The type
-   * in the tutorial should be either <code>GL_VERTEX_SHADER</code> or
-   * <code>GL_FRAGMENT_SHADER</code>.
-   *
-   * @param type   Type of the shader
+   * Sets the source code of this shader
+   * @param source GLSL Source Code for the shader
+   */
+  public void source(CharSequence source) {
+    glShaderSource(id, source);
+  }
+  
+  /**
+   * Compiles the shader and checks its status afterwards
+   * */
+  public void compile() {
+    glCompileShader(id);
+    checkStatus();
+  }
+  
+  /**
+   * Deletes the shader
+   */
+  public void delete() {
+    glDeleteShader(id);
+  }
+  
+  /**
+   * Creates a shader with specified type and source and compiles it.
+   * Currently, only supports vertex (GL_VERTEX_SHADER) and fragment (GL_FRAGMENT_SHADER) shaders.
+   * @param type Type of the shader
    * @param source Source of the shader
-   *
    * @return Compiled Shader from the specified source
    */
   public static Shader createShader(int type, CharSequence source) {
     Shader shader = new Shader(type);
     shader.source(source);
     shader.compile();
-    
     return shader;
   }
   
   /**
-   * Loads a shader from a file.
-   *
+   * Compiles a shader file
    * @param type Type of the shader
    * @param path File path of the shader
-   *
-   * @return Compiled Shader from specified file
+   * @return Compiled Shader from given file
    */
   public static Shader loadShader(int type, String path) {
     StringBuilder builder = new StringBuilder();
-    
     try (InputStream in = new FileInputStream(path);
          BufferedReader reader = new BufferedReader(new InputStreamReader(in))) {
       String line;
@@ -109,12 +87,21 @@ public class Shader {
         builder.append(line).append("\n");
       }
     } catch (IOException ex) {
-      throw new RuntimeException("Failed to load a shader file!"
-        + System.lineSeparator() + ex.getMessage());
+      throw new RuntimeException("Failed to load a shader file!" + System.getProperty("file.separator") + ex.getMessage());
     }
     CharSequence source = builder.toString();
-    
     return createShader(type, source);
+  }
+  
+  
+  /**
+   * Validates shader compilation
+   */
+  private void checkStatus() {
+    int status = glGetShaderi(id, GL_COMPILE_STATUS);
+    if (status != GL_TRUE) {
+      throw new RuntimeException(glGetShaderInfoLog(id));
+    }
   }
   
 }
