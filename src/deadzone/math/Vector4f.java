8ab09acac0aef49429be2de1544c1cd8ff582ac5
stuck1a <stuck1a@stuck1a.de>
2024-03-28 16:57:17 +0100

    Implemented (x,y,z,w)-Vector Fixed some comments in Vector3f

--- src/deadzone/math/Vector4f.java
+++ src/deadzone/math/Vector4f.java
@@ -1,30 +1,34 @@
 package deadzone.math;
 
 /**
- * This class represents a (x,y,z)-Vector based on float values.
+ * This class represents a (x,y,z,w)-Vector based on float values.
+ * To make it easier to use with GLSLs vec4, the scalars are labeled (x,y,z,w) instead of (x1,x2,x,3,x4).
  */
-public class Vector3f {
+public class Vector4f {
   
   public float x;
   public float y;
   public float z;
+  public float w;
   
   /**
    * Creates a new vector with 0 for all values
    */
-  public Vector3f() {
+  public Vector4f() {
     this.x = 0f;
     this.y = 0f;
     this.z = 0f;
+    this.w = 0f;
   }
   
   /**
    * Creates a new vector with the given values
    */
-  public Vector3f(float x, float y, float z) {
+  public Vector4f(float x, float y, float z, float w) {
     this.x = x;
     this.y = y;
     this.z = z;
+    this.w = w;
   }
   
   
@@ -33,7 +37,7 @@ public class Vector3f {
    * Calculates the squared scalar length of this vector
    */
   public float lengthSquared() {
-    return x * x + y * y + z * z;
+    return x * x + y * y + z * z + w * w;
   }
   
   
@@ -46,47 +50,37 @@ public class Vector3f {
   
   
   /**
-   * Adds this vector to another (x,y,z)-Vector
+   * Adds this vector to another (x,y,z,w)-Vector
    */
-  public Vector3f add(Vector3f vector) {
+  public Vector4f add(Vector4f vector) {
     float x = this.x + vector.x;
     float y = this.y + vector.y;
     float z = this.z + vector.z;
-    return new Vector3f(x, y, z);
+    float w = this.w + vector.w;
+    return new Vector4f(x, y, z, w);
   }
   
   
   /**
-   * Subtracts this vector from another (x,y,z)-Vector
+   * Subtracts this vector from another (x,y,z,w)-Vector
    */
-  public Vector3f subtract(Vector3f vector) {
+  public Vector4f subtract(Vector4f vector) {
     return this.add(vector.negate());
   }
   
   
   /**
-   * Calculates the dot product of this vector with another (x,y,z)-Vector
+   * Calculates the dot product of this vector with another (x,y,z,w)-Vector
    */
-  public float dot(Vector3f vector) {
-    return this.x * vector.x + this.y * vector.y + this.z * vector.z;
-  }
-  
-  
-  /**
-   * Calculates the cross product of this vector with another (x,y,z)-Vector
-   */
-  public Vector3f cross(Vector3f vector) {
-    float x = this.y * vector.z - this.z * vector.y;
-    float y = this.z * vector.x - this.x * vector.z;
-    float z = this.x * vector.y - this.y * vector.x;
-    return new Vector3f(x, y, z);
+  public float dot(Vector4f vector) {
+    return this.x * vector.x + this.y * vector.y + this.z * vector.z + this.w * vector.w;
   }
   
   
   /**
    * Negates this vector
    */
-  public Vector3f negate() {
+  public Vector4f negate() {
     return multiply(-1f);
   }
   
@@ -94,18 +88,19 @@ public class Vector3f {
   /**
    * Multiplies ("scales") this vector with a scalar
    */
-  public Vector3f multiply(float scalar) {
+  public Vector4f multiply(float scalar) {
     float x = this.x * scalar;
     float y = this.y * scalar;
     float z = this.z * scalar;
-    return new Vector3f(x, y, z);
+    float w = this.w * scalar;
+    return new Vector4f(x, y, z, w);
   }
   
   
   /**
    * Divides this vector with a scalar value
    */
-  public Vector3f divide(float scalar) {
+  public Vector4f divide(float scalar) {
     return multiply(1f / scalar);
   }
   
@@ -113,7 +108,7 @@ public class Vector3f {
   /**
    * Normalizes this vector (same direction with length of 1)
    */
-  public Vector3f normalize() {
+  public Vector4f normalize() {
     float length = length();
     return divide(length);
   }
