497ffff07401c674d57760f082c2c18cc4393cc7
stuck1a <stuck1a@stuck1a.de>
2024-03-31 17:35:36 +0200

    store changes to create dev branch

--- src/deadzone/scenes/Scene.java
+++ src/deadzone/scenes/Scene.java
@@ -5,7 +5,7 @@ package deadzone.scenes;
  */
 public enum Scene {
   NONE,        // Abstract placeholder scene
-  LAUNCHER,    // Initial application scene, player can login/register here, adjust basic settings and then start the game itself
+  LAUNCHER,    // Initial application scene, player can log in/register here, adjust basic settings and then start the game itself
   LOADING,     // Intermediate scene, might be used to load resources if necessary
   COMPOUND,    // Central and first in-game scene
   MAP,         // Accessed from the compound scene and used to start missions
